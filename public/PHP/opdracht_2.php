5d5a8bfa79ba7fd93ca3f4b64c0f1a31e77ac32a
jor697 <150015@csgaugustinus.nl>
2020-05-23 11:07:43 +0000

    commit

--- public/PHP/opdracht_2.php
+++ public/PHP/opdracht_2.php
@@ -5,7 +5,7 @@ include('opdracht_begin.php');
 TYP HIERONDER JOUW PHPCODE
 ****************************/
 
-$tekst="Ik heb een bijbaantje bij de Aldi. De Aldi betaalt goed.";
+$tekst="Ik heb een bijbaantje bij de Aldi. De Aldi betaalt goed";
 echo "$tekst";
 //$tekst=str_replace('Aldi','Albert Heijn',$tekst);
 echo '<h4>$tekst</h4>';
