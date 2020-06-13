4e203210522b877c065102e4b360b887db00ace2
jor697 <150015@csgaugustinus.nl>
2020-06-13 12:35:45 +0000

    commit

--- public/mijnProject/Defenders.php
+++ public/mijnProject/Defenders.php
@@ -11,12 +11,12 @@ else {
 $sql = "SELECT * FROM defenders";
 $records = mysqli_query($DBverbinding, $sql);
 $aantalOperators = mysqli_num_rows($records);
-//if (isset($_GET["nr"]) && $_GET["nr"] <= $aantalOperators && $_GET["nr"] > 0) {
-//    $operators = $_GET["nr"];
-//}
-//else {
+if (isset($_GET["nr"]) && $_GET["nr"] <= $aantalOperators && $_GET["nr"] > 0) {
+    $operators = $_GET["nr"];
+}
+else {
     $operators = 1;
-//}
+}
 
 $sql = "SELECT * FROM defenders WHERE icon = $operators";
 $record = mysqli_query($DBverbinding, $sql);
@@ -35,34 +35,34 @@ $operatorData = mysqli_fetch_assoc($record);
     <body style="background-color:rgb(67, 74, 84)">
         <div class="roster" id='roster' style='float:left;'>
         <ul>
-            <li><img src='images/kapkan.png' height=100px width=100px>
-            <img src='images/tachanka.png' height=100px width=100px></li>
-            <li><img src='images/bandit.png' height=100px width=100px>
-            <img src='images/jager.png' height=100px width=100px></li>
-            <li><img src='images/rook.png' height=100px width=100px>
-            <img src='images/doc.png' height=100px width=100px></li>
-            <li><img src='images/pulse.png' height=100px width=100px>
-            <img src='images/castle.png' height=100px width=100px></li>
-            <li><img src='images/smoke.png' height=100px width=100px>
-            <img src='images/mute.png' height=100px width=100px></li>
-            <li><img src='images/frost.png' height=100px width=100px>
-            <img src='images/valkyrie.png' height=100px width=100px></li>
-            <li><img src='images/caveira.png' height=100px width=100px>
-            <img src='images/echo.png' height=100px width=100px></li>
-            <li><img src='images/mira.png' height=100px width=100px>
-            <img src='images/lesion.png' height=100px width=100px></li>
-            <li><img src='images/ela.png' height=100px width=100px>
-            <img src='images/vigil.png' height=100px width=100px></li>
-            <li><img src='images/alibi.png' height=100px width=100px>
-            <img src='images/maestro.png' height=100px width=100px></li>
-            <li><img src='images/clash.png' height=100px width=100px>
-            <img src='images/kaid.png' height=100px width=100px></li>
-            <li><img src='images/mozzie.png' height=100px width=100px>
-            <img src='images/warden.png' height=100px width=100px></li>
-            <li><img src='images/goyo.png' height=100px width=100px>
-            <img src='images/wamai.png' height=100px width=100px></li>
-            <li><img src='images/oryx.png' height=100px width=100px>
-            <img src='images/melusi.png' height=100px width=100px></li>
+            <li><a href='Defenders.php/?nr=1'><img src='images/kapkan.png' height=100px width=100px></a>
+            <a href='Defenders.php/?nr=2'><img src='images/tachanka.png' height=100px width=100px></a></li>
+            <li><a href='Defenders.php/?nr=3'><img src='images/bandit.png' height=100px width=100px></a>
+            <a href='Defenders.php/?nr=4'><img src='images/jager.png' height=100px width=100px></a></li>
+            <li><a href='Defenders.php/?nr=5'><img src='images/rook.png' height=100px width=100px></a>
+            <a href='Defenders.php/?nr=6'><img src='images/doc.png' height=100px width=100px></a></li>
+            <li><a href='Defenders.php/?nr=7'><img src='images/pulse.png' height=100px width=100px></a>
+            <a href='Defenders.php/?nr=8'><img src='images/castle.png' height=100px width=100px></li></a>
+            <li><a href='Defenders.php/?nr=9'><img src='images/smoke.png' height=100px width=100px></a>
+            <a href='Defenders.php/?nr=10'><img src='images/mute.png' height=100px width=100px></a></li>
+            <li><a href='Defenders.php/?nr=11'><img src='images/frost.png' height=100px width=100px></a>
+            <a href='Defenders.php/?nr=12'><img src='images/valkyrie.png' height=100px width=100px></a></li>
+            <li><a href='Defenders.php/?nr=13'><img src='images/caveira.png' height=100px width=100px></a>
+            <a href='Defenders.php/?nr=14'><img src='images/echo.png' height=100px width=100px></a></li>
+            <li><a href='Defenders.php/?nr=15'><img src='images/mira.png' height=100px width=100px></a>
+            <a href='Defenders.php/?nr=16'><img src='images/lesion.png' height=100px width=100px></a></li>
+            <li><a href='Defenders.php/?nr=17'><img src='images/ela.png' height=100px width=100px></a>
+            <a href='Defenders.php/?nr=18'><img src='images/vigil.png' height=100px width=100px></a></li>
+            <li><a href='Defenders.php/?nr=19'><img src='images/alibi.png' height=100px width=100px></a>
+            <a href='Defenders.php/?nr=20'><img src='images/maestro.png' height=100px width=100px></a></li>
+            <li><a href='Defenders.php/?nr=21'><img src='images/clash.png' height=100px width=100px></a>
+            <a href='Defenders.php/?nr=22'><img src='images/kaid.png' height=100px width=100px></a></li>
+            <li><a href='Defenders.php/?nr=23'><img src='images/mozzie.png' height=100px width=100px></a>
+            <a href='Defenders.php/?nr=24'><img src='images/warden.png' height=100px width=100px></a></li>
+            <li><a href='Defenders.php/?nr=25'><img src='images/goyo.png' height=100px width=100px></a>
+            <a href='Defenders.php/?nr=26'><img src='images/wamai.png' height=100px width=100px></a></li>
+            <li><a href='Defenders.php/?nr=27'><img src='images/oryx.png' height=100px width=100px></a>
+            <a href='Defenders.php/?nr=28'><img src='images/melusi.png' height=100px width=100px></a></li>
         </ul>    
         </div>
         <div style="width: 0px; border: 3px solid rgb(47, 54, 64);; border-radius: 50px; float:left; height:1450px; margin:10px; margin-top:15px"></div>
--- public/mijnProject/Most.php
+++ public/mijnProject/Most.php
@@ -18,16 +18,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
-        <div class='popular'>
-            <div class='images'>
-                <ul>
-                    <!--<li><a href="Defenders.php"><img src="images/jager.png" class='size' width='150' height='150'></a></li>
-                    <li><a href="Defenders.php"><img src="images/doc.png" class='size' width= '150' height= '150'></a></li>
-                    <li><a href="Defenders.php"><img src="images/caveira.png" class='size' width='150' height='150'></a></li>
-                    <li><a href="Attackers.php"><img src="images/jackal.png" class='size' width='150' height='150'></a></li>-->
-                    <li><a href="Defenders.php"><img src="images/lesion.png" class='size' width='150' height='150'></a></li>
-                </ul>
-            </div>
-        </div>
+        <ul class='popular'>
+            <li><a href="Defenders.php"><img src="images/jager.png" class='size'  width='150' height='150'></a></li>
+            <li><a href="Defenders.php"><img src="images/doc.png" class='size' width= '150' height= '150'></a></li>
+            <li><a href="Defenders.php"><img src="images/caveira.png" class='size' width='150' height='150'></a></li>
+            <li><a href="Attackers.php"><img src="images/jackal.png" class='size' width='150' height='150'></a></li>
+            <li><a href="Defenders.php"><img src="images/lesion.png" class='size' width='150' height='150'></a></li>
+        </ul>
     </body>
 </html>
